8d70827ce0b4fcfb9e7cb736997ba90013b5b0e3
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-08 16:35:03 +0100

    Testing vuex persist
    Installed
     - vuex-persist
    
    Added
     - New view TestArea.vue
     - New route for TestArea component
     - Function to add number 1 to someArray in vuex
     - /todo to .gitignore
     - Added setup in store.js
    
    Removed "HelloWorld copy.vue" component

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex)
 
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+})
+
 export default new Vuex.Store({
   state: {
+    someArray: []
   },
   mutations: {
+    initArray(state) {
+      state.someArray = [1, 2, 3]
+    },
+    addNumber(state, number) {
+      state.someArray.push(Number(number))
+    },
+    resetStorage(state) {
+      state.someArray = []
+    },
   },
   actions: {
   },
-  modules: {
-  }
+  plugins: [vuexLocalStorage.plugin]
 })
---
+++ src/views/TestArea.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <div>
+      <button @click="$store.commit('initArray')">
+        I initialize someArray with 1,2,3
+      </button>
+    </div>
+    <div>
+      <input v-model="testNumber" />
+      <button @click="$store.commit('addNumber', testNumber)">
+        Add number to left into someArray
+      </button>
+    </div>
+    <div>This is someArray: {{ this.$store.state.someArray }}</div>
+    <div>
+      <button @click="$store.commit('resetStorage')">
+        Reset local storage
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* + * {
+  margin-top: 1rem;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      testNumber: 0,
+    };
+  },
+};
+</script>
