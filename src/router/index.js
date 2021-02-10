d2f6ff293a58c202e0008131e3438cd22d88f597
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-10 10:48:03 +0100

    this branch gonna die

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import TestArea from '../views/TestArea.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import TestArea from "../views/TestArea.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
-    path: '/testarea',
-    name: 'TestArea',
-    component: TestArea
-  }
-]
+    path: "/testarea",
+    name: "TestArea",
+    component: TestArea,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
