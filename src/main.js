3db6bd170feb9582c0066cadd90baaaf20a10cea
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-11 11:38:27 +0100

    New design system implementation
    In previous commits we tried to implement scss variables globally in
    main.scss; but variables defined in main.scss does not get applied
    globally, scss variables are only used during the import of that file
    
    Solution in this case is to define global css variables with :root
    selector in main.css and import that main.css instead of main.scss
    in main.js
    
    Removing sass from project as team is not using any sass function

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import './main.scss'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import "./main.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
