fd830d7c64bad410c2a7b43258e21abc6d15cf57
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-13 21:02:30 +0100

    Clean some code

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+import Calendar from '../views/Calendar'
 import ListOverview from '../views/ListOverview.vue'
+import ListView from '../views/ListView'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 import TestArea from '../views/TestArea.vue'
-import ListView from '../views/ListView'
-import Calendar from '../views/Calendar'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,16 @@ const routes = [
     path: '/',
     name: 'ListOverview',
     component: ListOverview
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
+  },
+  {
+    path: '/list/:listName',
+    name: 'ListView',
+    component: ListView
   },
   {
     path: '/login',
@@ -26,19 +37,9 @@ const routes = [
     component: SignUp
   },
   {
-    path: '/calendar',
-    name: 'Calendar',
-    component: Calendar
-  },
-  {
     path: '/testarea',
     name: 'TestArea',
     component: TestArea
-  },
-  {
-    path: '/list/:listName',
-    name: 'ListView',
-    component: ListView
   }
 ]
 
