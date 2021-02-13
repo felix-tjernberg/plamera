60c36c53b93647097a7000c096e5e33b75e4cb7d
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-13 17:54:53 +0100

    Update project configuration
    Moved elint configuration from eslintrc.json file into package.json
    
    Added settings.json to make sure everyone has correct vscode settings
    
    Added jsconfig.json to remove vetur warning
    
    Made sure all files had been prettier formatted

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,8 @@
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >
+        >vue-cli documentation
+      </a>
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
@@ -15,40 +15,41 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
-        >
+          >babel
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
           target="_blank"
           rel="noopener"
-          >pwa</a
-        >
+          >pwa
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
-          >router</a
-        >
+          >router
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
           target="_blank"
           rel="noopener"
-          >vuex</a
-        >
+          >vuex
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-          >eslint</a
         >
+          eslint
+        </a>
       </li>
     </ul>
     <h3>Essential Links</h3>
