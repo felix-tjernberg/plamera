587efd52777f30c4401ed035730c952bd273cdee
Robin Esbjörnsson <robin.esbjornsson@iths.se>
2021-02-15 14:58:39 +0100

    Add component AddTaskButton.vue

---
+++ src/components/AddTaskButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="addTaskButton"></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.addTaskButton {
+  background-image: Url("~@/assets/Plus.svg");
+  background-position-x: center;
+  background-position-y: center;
+  background-repeat: no-repeat;
+  border-radius: 30px;
+  background-color: #5db075;
+  border: none;
+  height: 58px;
+  width: 58px;
+}
+</style>
--- src/views/TestArea.vue
+++ src/views/TestArea.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h1>Testing Area</h1>
+    <AddTaskButton> </AddTaskButton>
   </div>
 </template>
+
+<script>
+import AddTaskButton from '@/components/AddTaskButton.vue'
+
+export default {
+  components: {
+    AddTaskButton
+  }
+  
+}
+</script>
+
