8bcf1217280beb8c14ed5b5a6d5efc8842c21ee8
FilleFLEX <felix.tjernberg@hotmail.se>
2021-02-15 13:59:56 +0100

    Cleanup after revert

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div id="nav">
-      <router-link to="/"></router-link> 
-      <router-link to="/about"></router-link>
-    </div>
-    <router-view/>
-=======
     <nav>
       <router-link to="/">List Overview</router-link>|
       <router-link to="/login">Login</router-link>|
@@ -17,46 +10,29 @@
       <router-link to="/testarea">Test Area</router-link>
     </nav>
     <router-view />
->>>>>>> c4a2882f5c479a820c2a4df22977b9bd3a6b6f35
   </div>
 </template>
 
 <style>
-<<<<<<< HEAD
-/*#app {
+/* Tempory styling of a tempory nav */
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-=======
-  /* Tempory styling of a tempory nav */
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
->>>>>>> c4a2882f5c479a820c2a4df22977b9bd3a6b6f35
 
-  nav {
-    padding: 30px;
-  }
+nav {
+  padding: 30px;
+}
 
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-<<<<<<< HEAD
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
-}*/
-=======
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
->>>>>>> c4a2882f5c479a820c2a4df22977b9bd3a6b6f35
+}
 </style>
