09020ee73b73a8e6f8cff49d7ebf7d9e5bd52687
Amanda C.Watson <amandacwatson88@gmail.com>
2021-03-02 17:01:42 +0100

    Add watcher to input Email and Password in Login and SignUp pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,20 @@
     <!--MOBILE-->
     <div class="Mobilecontainer">
       <h1>Login</h1>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input type="text" placeholder="Email" v-model="email" required />
+      <br />
+      <span class="span-validation" v-if="message.email">{{
+        message.email
+      }}</span>
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="password"
+        required
+      /><br />
+      <span class="span-validation" v-if="message.password">{{
+        message.password
+      }}</span>
       <input
         type="submit"
         value="Log in"
@@ -19,8 +31,20 @@
     <div class="Desktopcontainer">
       <h1 class="LoginHeader">Login</h1>
       <div class="inputs">
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Email" v-model="email" required />
+        <br />
+        <span class="span-validation" v-if="message.email">{{
+          message.email
+        }}</span>
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          required
+        /><br />
+        <span class="span-validation" v-if="message.password">{{
+          message.password
+        }}</span>
       </div>
       <input
         type="submit"
@@ -37,15 +61,60 @@
 
 <script>
   export default {
+    data() {
+      return {
+        password: '',
+        email: '',
+        message: []
+      }
+    },
+    watch: {
+      password(value) {
+        this.password = value
+        this.PasswordValidation(value)
+      },
+      email(value) {
+        this.email = value
+        this.EmailValidation(value)
+      }
+    },
     methods: {
+      EmailValidation(value) {
+        if (/^\w+([-]?\w+)*@\w+([-]?\w+)*(\w{2,3})+$/.test(value)) {
+          this.message['email'] = 'Required'
+        } else {
+          this.message['email'] = 'Please, enter a valid email'
+        }
+      },
+      PasswordValidation(value) {
+        
+        if (value.length < 8) {
+          this.message['password'] =
+            'Your password needs to be 8 characters'
+        } else {
+          this.message['password'] = 'Required'
+        }
+      },
       goToLandingpage() {
-        this.$router.push('/')
+        this.$router.push('/signup')
       }
     }
   }
 </script>
 
 <style>
+  .span-validation {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-start;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 19px;
+    color: #ED2121;
+    padding-top: 0px;
+    margin-top: 0px;
+  }
   @media only screen and (min-device-width: 1000px) {
   }
 </style>
